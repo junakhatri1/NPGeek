d89210f888f71c0aeb96359365285a53eca835a4
William Fridlund <wfridlund@gmail.com>
2017-07-05 13:21:14 -0400

    Made some tests

---
+++ capstone-project/Capstone.Web.Tests/DAL/SurveySqlDALTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Capstone.Web.DAL;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using Capstone.Web.Models;
+
+namespace Capstone.Web.Tests.DAL
+{
+    [TestClass]
+    public class SurveySqlDALTests
+    {
+
+        string connectionString = @"Data Source=.\sqlexpress;Initial Catalog=NPGeek;User ID=te_student;Password=sqlserver1";
+        int numOfSurveys = 0;
+
+        [TestMethod]
+        public void GetAllSurveys()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd;
+                conn.Open();
+                cmd = new SqlCommand("SELECT Count(*) FROM survey_result;", conn);
+                numOfSurveys = (int)cmd.ExecuteScalar();
+            }
+            SurveySqlDAL surveySql = new SurveySqlDAL(connectionString);
+
+            List<Survey> surveys = surveySql.GetAllSurveys();
+
+            Assert.IsNotNull(surveys);
+            Assert.AreEqual(numOfSurveys, surveys.Count);
+        }
+    }
+}
---
+++ capstone-project/Capstone.Web.Tests/DAL/WeatherSqlDALTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Capstone.Web.DAL;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using Capstone.Web.Models;
+
+namespace Capstone.Web.Tests.DAL
+{
+    [TestClass]
+    public class WeatherSqlDALTests
+    {
+        string connectionString = @"Data Source=.\sqlexpress;Initial Catalog=NPGeek;User ID=te_student;Password=sqlserver1";
+
+        [TestMethod]
+        public void GetFiveDaysWeatherTests()
+        {
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd;
+                conn.Open();
+                cmd = new SqlCommand("SELECT Count(*) FROM weather WHERE parkCode = 'CVNP';", conn);
+            }
+            WeatherSqlDAL weatherSql = new WeatherSqlDAL(connectionString);
+
+            List<Weather> weathers = weatherSql.GetFiveDaysWeather("CVNP");
+
+            Assert.IsNotNull(weathers);
+            Assert.AreEqual(5, weathers.Count);
+        }
+    }
+}
